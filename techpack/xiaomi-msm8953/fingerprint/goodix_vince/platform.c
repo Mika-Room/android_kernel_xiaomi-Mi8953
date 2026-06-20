29825cd1488e732c3b4af64bb23b3e610ac5e72f
Mika <enashinonome264@gmail.com>
2026-06-20 19:12:12 -0400

    techpack: xiaomi-msm8953: fp: goodix_vince: Avoid function conflicts

--- techpack/xiaomi-msm8953/fingerprint/goodix_vince/platform.c
+++ techpack/xiaomi-msm8953/fingerprint/goodix_vince/platform.c
@@ -76,14 +76,14 @@ int vince_gf_parse_dts(struct gf_dev *gf_dev)
 	pr_warn("--------gf_parse_dts start  haijun.--------\n");
 
 	/*get reset resource*/
-	rc = gf3208_request_named_gpio(gf_dev, "goodix,gpio_reset", &gf_dev->reset_gpio);
+	rc = vince_gf3208_request_named_gpio(gf_dev, "goodix,gpio_reset", &gf_dev->reset_gpio);
 	if (rc) {
 		gf_dbg("Failed to request RESET GPIO. rc = %d\n", rc);
 		return -EPERM;
 	}
 
 	/*get irq resourece*/
-	rc = gf3208_request_named_gpio(gf_dev, "goodix,gpio_irq", &gf_dev->irq_gpio);
+	rc = vince_gf3208_request_named_gpio(gf_dev, "goodix,gpio_irq", &gf_dev->irq_gpio);
 	if (rc) {
 		gf_dbg("Failed to request IRQ GPIO. rc = %d\n", rc);
 		return -EPERM;
@@ -104,10 +104,10 @@ int vince_gf_parse_dts(struct gf_dev *gf_dev)
 		gf_dev->pinctrl_state[i] = state;
 	}
 
-	rc = select_pin_ctl(gf_dev, "goodixfp_reset_active");
+	rc = vince_select_pin_ctl(gf_dev, "goodixfp_reset_active");
 	if (rc)
 		goto exit;
-	rc = select_pin_ctl(gf_dev, "goodixfp_irq_active");
+	rc = vince_select_pin_ctl(gf_dev, "goodixfp_irq_active");
 	if (rc)
 		goto exit;
 #endif
@@ -170,12 +170,12 @@ static int hw_reset(struct  gf_dev *gf_dev)
 	struct device *dev = &gf_dev->spi->dev;
 
 #ifdef ENABLE_PINCTRL
-	int rc = select_pin_ctl(gf_dev, "goodixfp_reset_reset");
+	int rc = vince_select_pin_ctl(gf_dev, "goodixfp_reset_reset");
 	if (rc)
 		goto exit;
 	mdelay(3);
 
-	rc = select_pin_ctl(gf_dev, "goodixfp_reset_active");
+	rc = vince_select_pin_ctl(gf_dev, "goodixfp_reset_active");
 	if (rc)
 		goto exit;
 #else
