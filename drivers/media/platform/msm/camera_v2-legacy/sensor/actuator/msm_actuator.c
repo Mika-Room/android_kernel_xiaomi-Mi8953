d7042809bb5974f94b4d6f766ea8cab85ebfd046
mizukanaAlt <rina301087@gmail.com>
2026-06-20 20:19:29 +0000

    media: camera-v2-legacy: Import mido unified changes

--- drivers/media/platform/msm/camera_v2-legacy/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2-legacy/sensor/actuator/msm_actuator.c
@@ -33,6 +33,11 @@ DEFINE_MSM_MUTEX(msm_actuator_mutex);
 #define PARK_LENS_MID_STEP 5
 #define PARK_LENS_SMALL_STEP 3
 #define MAX_QVALUE 4096
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_MIDO)
+#define PARK_LENS_QUIET_UPPER_CODE 400
+#define PARK_LENS_QUIET_LOWER_CODE 200
+#define PARK_LENS_QUIET_STEP 25
+#endif
 
 static struct v4l2_file_operations msm_actuator_v4l2_subdev_fops;
 static int32_t msm_actuator_power_up(struct msm_actuator_ctrl_t *a_ctrl);
@@ -842,6 +847,7 @@ static int32_t msm_actuator_park_lens(struct msm_actuator_ctrl_t *a_ctrl)
 	int32_t rc = 0;
 	uint16_t next_lens_pos = 0;
 	struct msm_camera_i2c_reg_setting reg_setting;
+	int is_mido = (xiaomi_msm8953_mach_get() == XIAOMI_MSM8953_MACH_MIDO);
 
 	a_ctrl->i2c_tbl_index = 0;
 	if ((a_ctrl->curr_step_pos > a_ctrl->total_steps) ||
@@ -861,27 +867,36 @@ static int32_t msm_actuator_park_lens(struct msm_actuator_ctrl_t *a_ctrl)
 	next_lens_pos = a_ctrl->step_position_table[a_ctrl->curr_step_pos];
 	while (next_lens_pos) {
 		/* conditions which help to reduce park lens time */
-		if (next_lens_pos > (a_ctrl->park_lens.max_step *
-			PARK_LENS_LONG_STEP)) {
-			next_lens_pos = next_lens_pos -
-				(a_ctrl->park_lens.max_step *
-				PARK_LENS_LONG_STEP);
-		} else if (next_lens_pos > (a_ctrl->park_lens.max_step *
-			PARK_LENS_MID_STEP)) {
-			next_lens_pos = next_lens_pos -
-				(a_ctrl->park_lens.max_step *
-				PARK_LENS_MID_STEP);
-		} else if (next_lens_pos > (a_ctrl->park_lens.max_step *
-			PARK_LENS_SMALL_STEP)) {
-			next_lens_pos = next_lens_pos -
-				(a_ctrl->park_lens.max_step *
-				PARK_LENS_SMALL_STEP);
-		} else {
-			next_lens_pos = (next_lens_pos >
-				a_ctrl->park_lens.max_step) ?
-				(next_lens_pos - a_ctrl->park_lens.
-				max_step) : 0;
-		}
+		if (is_mido) {
+            if (next_lens_pos > PARK_LENS_QUIET_UPPER_CODE)
+                next_lens_pos = PARK_LENS_QUIET_UPPER_CODE;
+            else if (next_lens_pos > PARK_LENS_QUIET_LOWER_CODE)
+                next_lens_pos -= PARK_LENS_QUIET_STEP;
+            else
+                next_lens_pos = 0;
+        } else {
+            if (next_lens_pos > (a_ctrl->park_lens.max_step *
+                PARK_LENS_LONG_STEP)) {
+                next_lens_pos = next_lens_pos -
+                    (a_ctrl->park_lens.max_step *
+                    PARK_LENS_LONG_STEP);
+            } else if (next_lens_pos > (a_ctrl->park_lens.max_step *
+                PARK_LENS_MID_STEP)) {
+                next_lens_pos = next_lens_pos -
+                    (a_ctrl->park_lens.max_step *
+                    PARK_LENS_MID_STEP);
+            } else if (next_lens_pos > (a_ctrl->park_lens.max_step *
+                PARK_LENS_SMALL_STEP)) {
+                next_lens_pos = next_lens_pos -
+                    (a_ctrl->park_lens.max_step *
+                    PARK_LENS_SMALL_STEP);
+            } else {
+                next_lens_pos = (next_lens_pos >
+                    a_ctrl->park_lens.max_step) ?
+                    (next_lens_pos - a_ctrl->park_lens.
+                    max_step) : 0;
+            }
+        }
 		a_ctrl->func_tbl->actuator_parse_i2c_params(a_ctrl,
 			next_lens_pos, a_ctrl->park_lens.hw_params,
 			a_ctrl->park_lens.damping_delay);
