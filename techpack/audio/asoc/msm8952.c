f17a76365ea88aa43b4eaa4e85c7f0e56316f41a
amemika <297562698+AmeMika@users.noreply.github.com>
2026-07-01 00:08:39 +0000

    techpack: audio: Add btn_low[] and btn_high[] overrides for Rosy

--- techpack/audio/asoc/msm8952.c
+++ techpack/audio/asoc/msm8952.c
@@ -1647,6 +1647,18 @@ static void *def_msm8952_wcd_mbhc_cal(void)
 
 #if IS_ENABLED(CONFIG_MACH_XIAOMI_MSM8953)
 	switch (xiaomi_msm8953_mach_get()) {
+		case XIAOMI_MSM8953_MACH_ROSY:
+			btn_low[0] = 25;
+			btn_high[0] = 75;
+			btn_low[1] = 200;
+			btn_high[1] = 225;
+			btn_low[2] = 325;
+			btn_high[2] = 450;
+			btn_low[3] = 500;
+			btn_high[3] = 510;
+			btn_low[4] = 530;
+			btn_high[4] = 540;
+			break;
 		case XIAOMI_MSM8953_MACH_YSL:
 			btn_low[0] = 100;
 			btn_high[0] = 100;
